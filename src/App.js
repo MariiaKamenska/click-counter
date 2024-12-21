a2a96026686c56dade522084f11a152c6a6838be
MariiaKamenska <kamenskaam53@gmail.com>
2024-12-21 19:59:31 +0200

    counter done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>Hi, React!</div>;
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  if (count < 0) {
+    // alert("no");
+    reset();
+  }
+   return (
+    <div style={styles.container}> 
+      <h1 style={styles.heading}> Лічильник кліків</h1>
+      <p style={styles.counter}>кількість кліків: {count}</p>
+      <div>
+        <button style={styles.button} onClick = {increase}>Додати</button>
+        <button style={styles.button} onClick = {decrease}> Зменшити</button>
+        <button style={styles.button} onClick = {reset}>Скинути</button>
+
+
+      </div>
+    </div>
+  );
+}
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px'
+  },
+  heading: {
+    color: "#007bff"
+  },
+  counter: {
+    fontSize: '24px',
+    margin: '20px 0',
+  },
+  button: {
+    padding: '10px 20px',
+    margin: '5px',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    background: '#007bff',
+    color: '#fff'
+  }
 }
 
 export default App;
